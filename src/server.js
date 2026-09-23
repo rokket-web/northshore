3c0dd0c81fa37b0235d6b10df348bc15b17c6485
Rokket Web Development <60049291+rokket-web@users.noreply.github.com>
2026-09-23 12:06:00 -0700

    updated landing

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require('express');
 const cors = require('cors');
 const config = require('./config');
@@ -9,6 +10,12 @@ const app = express();
 // needs to be reachable cross-origin. TODO: once the Webflow domain is final, restrict
 // this to that origin instead of allowing all.
 app.use(cors());
+
+app.use(express.static(path.join(__dirname, '..', 'public')));
+
+app.get('/quiz', (req, res) => {
+  res.sendFile(path.join(__dirname, '..', 'webflow-quiz', 'spiritual-gifts-quiz.html'));
+});
 
 app.get('/health', (req, res) => res.json({ status: 'ok' }));
 
